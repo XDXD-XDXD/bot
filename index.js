5decaf846ce414482a1fcadf4fb624e8dc2518e6
- <saumitraverma04@gmail.com>
2021-03-03 17:20:13 +0530

    implement the basic things

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+	console.log(message.content);
+});
+
+client.login(config.token);
