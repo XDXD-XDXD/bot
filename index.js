b4ff48a201d53e6fb615cb0f231ed37da6ade985
- <saumitraverma04@gmail.com>
2021-03-05 12:15:04 +0530

    Added !server command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
+
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -11,7 +12,12 @@ client.on('message', message => {
 		// send back "Pong." to the channel the message was sent in
 		message.channel.send('Pong.');
 	}
+	else if (message.content === `${prefix}server`) {
+		message.channel.send(`This server's name is: ${message.guild.name}`);
+	}
+
 	console.log(message.content);
 });
 
-client.login(config.token);
+client.login(token);
+
