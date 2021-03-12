d8f3be76049e5e7c52dc238d452c470adfba738d
- <>
2021-03-12 23:09:54 +0530

    add some things

--- index.js
+++ index.js
@@ -5,18 +5,30 @@ const { prefix, token } = require('./config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
+	let a = client.users.fetch(659047804622077953);
+	console.log(a);
+
 });
 
 client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
 	if (message.content === '!ping') {
 		// send back "Pong." to the channel the message was sent in
 		message.channel.send('Pong.');
 	}
 	else if (message.content === `${prefix}server`) {
-		message.channel.send(`This server's name is: ${message.guild.name}`);
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+
+	}
+	else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	}
 
-	console.log(message.content);
+
 });
 
 client.login(token);
